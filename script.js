9908cb574422d61c355b4828fe33f0358d6cd779
Даниил Лавров <84658360+LavDaDi@users.noreply.github.com>
2023-12-04 00:45:14 +0300

    [papalupa

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const buttonElem = document.querySelector(".go");
-const modalElem = document.querySelector('.modal');
+const qr1_t1_t = document.querySelector('.modal');
 
-modalElem.style.cssText = `
-    display: flex;
-    visibility: hidden;
-    opacity: 0;
-    transition: opacity 300ms ease-in-out;
-`;
-
-const openModal = () => {
-    modalElem.style.visibility = 'visible';
-    modalElem.style.opacity = 1;
+go1.onclick = function() {
+    var val = document.getElementById('input_phone_text').value;
+    if (val === 'Куранты' || val === 'куранты'){
+        qr1_t1_t.style.cssText = `
+        display: flex;
+        visibility = 'visible';
+        opacity = 1;
+        transition: opacity 300ms ease-in-out;
+    `;
+    
+   // const openModal = () => {
+    //    qr1_t1_t.style.visibility = 'visible';
+   //     qr1_t1_t.style.opacity = 1;
+   // };
+    
+  //  buttonElem.addEventListener('click', openModal);
+    } else{
+        alert('ti lox')
+    }
+    
 };
-
-buttonElem.addEventListener('click', openModal);
