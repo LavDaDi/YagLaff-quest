12dd79f501445954ca7e31becdeab4ead6993048
Даниил Лавров <84658360+LavDaDi@users.noreply.github.com>
2023-12-04 19:51:18 +0300

    new functions

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const buttonElem = document.querySelector(".go");
-const modalElem = document.querySelector('.modal');
+const qr1_t1_t = document.querySelector('.modal');
+const qr1_t1_f = document.querySelector('.modal2');
 
-modalElem.style.cssText = `
-    display: flex;
-    visibility: hidden;
-    opacity: 0;
-    transition: opacity 300ms ease-in-out;
-`;
 
-const openModal = () => {
-    modalElem.style.visibility = 'visible';
-    modalElem.style.opacity = 1;
+go1.onclick = function() {
+    var val = document.getElementById('input_phone_text').value;
+    if (val === inp || val === inp_m){
+        qr1_t1_t.style.cssText = `
+        display: flex;
+        visibility = 'visible';
+        opacity = 1;
+        transition: opacity 300ms ease-in-out;
+    `;
+    
+   // const openModal = () => {
+    //    qr1_t1_t.style.visibility = 'visible';
+   //     qr1_t1_t.style.opacity = 1;
+   // };
+    
+  //  buttonElem.addEventListener('click', openModal);
+    } else{
+        qr1_t1_f.style.cssText = `
+        display: flex;
+        visibility = 'visible';
+        opacity = 1;
+        transition: opacity 300ms ease-in-out;
+    `}
+    
 };
-
-buttonElem.addEventListener('click', openModal);
